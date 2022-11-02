10d1b7baafdb12fabdccd5c04498b92d06f3e166
Kankit40 <Kankit40@gmail.com>
2022-11-02 18:03:37 +0530

    Added : UC1 code to check present or absent

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,7 +1,15 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee wage with Object oriented programming");
+
+        int a = (int)(Math.random() * 2);
+        if (a == 1){
+            System.out.println("Employee is Present");
+        }else {
+            System.out.println("Employee is Absent");
+        }
+
     }
 }
